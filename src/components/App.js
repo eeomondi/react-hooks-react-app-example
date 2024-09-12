1de4c61a1704c0dddc56819041291150d5ab014e
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-12 15:53:24 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ExampleComponent from "./ExampleComponent";
-import TestComponent from "./TestComponent";
+import React from 'react';
+import Navbar from './Navbar'; // Adjust the path if necessary
+import Home from './Home'; // Adjust the path if necessary
+import About from './About'; // Import the newly created About component
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Home />
+      <About />
       <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
@@ -13,10 +17,9 @@ function App() {
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
-      <ExampleComponent />
-      <TestComponent />
     </div>
   );
 }
 
 export default App;
+
