352bf0fba65716fc85294407c5917ef2749c8da5
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-12 15:56:48 +0300

    commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,15 +2,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App'; // Ensure this path matches your actual file structure
 
-test('renders Navbar, Home, and About components', () => {
-  const { getByText, getByTestId } = render(<App />);
-  
-  expect(getByText(/About/i)).toBeInTheDocument();
-  expect(getByText(/Now/i)).toBeInTheDocument();
-  expect(getByText(/In React apps/i)).toBeInTheDocument();
-  // Additional assertions based on your needs
-});
-
 test('should include "Now" in the header instead of a time', () => {
   render(<App />);
   expect(screen.getAllByText(/Now/i)).toBeInTheDocument();
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import Navbar from './Navbar'; // Adjust the path if necessary
-import Home from './Home'; // Adjust the path if necessary
-import About from './About'; // Import the newly created About component
+import React from "react";
+import ExampleComponent from "./ExampleComponent";
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <About />
       <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
@@ -17,9 +12,10 @@ function App() {
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
+      <ExampleComponent />
+      <TestComponent />
     </div>
   );
 }
 
 export default App;
-
